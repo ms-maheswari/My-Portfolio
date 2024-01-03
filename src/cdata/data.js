d43692a574676986747b24f41d7c6d7ef04d8d91
ms-maheswari <maheswarim.ug.21.cs@francisxavier.ac.in>
2024-01-03 11:43:36 +0530

    Add Coding Ninjas in progress

--- src/cdata/data.js
+++ src/cdata/data.js
@@ -4,6 +4,8 @@ import Leetcode from '../assets/progress/Leetcode.png'
 import leeticon from '../assets/progress/leeticon.png'
 import Geeks from '../assets/progress/Geeks.png'
 import gfgicon from '../assets/progress/gfgicon.png'
+import Ninjas from '../assets/progress/Ninjas.png'
+import cnicon from '../assets/progress/cnicon.svg'
 import Github from '../assets/progress/Github.png'
 import giticon from '../assets/progress/giticon.png'
 import Linkedin from '../assets/progress/Linkedin.png'
@@ -34,13 +36,21 @@ export const data=[
     },
     {
         id:4,
+        name:"Coding Ninjas",
+        image:Ninjas,
+        link:"https://www.codingninjas.com/studio/profile/0b5455c1-3b3e-4483-8d8e-76073146c86c",
+        icon: cnicon,
+    },
+
+    {
+        id:5,
         name:"Github",
         image:Github,
         link:"https://github.com/ms-maheswari",
         icon: giticon,
     },
     {
-        id:5,
+        id:6,
         name:"LinkedIn",
         image:Linkedin,
         link:"https://linkedin.com/in/maheswari-m-00b53a247",
